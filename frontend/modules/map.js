0dcd77aca8f45d106748587ea8c274274e8890a3
Simon Fallon <sfallong@eafit.edu.co>
2026-01-19 19:27:52 -0500

    refactor: add POI to map

--- frontend/modules/map.js
+++ frontend/modules/map.js
@@ -115,15 +115,11 @@ function initMap(center, zoom) {
     }
     map = L.map("mapContainer").setView(center, zoom);
 
-    L.tileLayer(
-      "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
-      {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: "abcd",
-        maxZoom: 20,
-      },
-    ).addTo(map);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      maxZoom: 19,
+    }).addTo(map);
   } else {
     map.setView(center, zoom);
     // Clear layers
